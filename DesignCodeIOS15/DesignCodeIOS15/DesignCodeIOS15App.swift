c63f3d1c01e5b55c1629b37145519da4b0d9823a
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-04 19:31:03 +0300

    added a project

---
+++ DesignCodeIOS15/DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by Hussain Almousawi on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
